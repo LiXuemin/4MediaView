d99e0d2485a594c9b70bc33d83236cc2f1693e27
lixuemin <lxmheida@gmail.com>
2020-04-29 21:17:52 +0800

    init s3 api restcontroller

---
+++ S3-FastDFS/src/main/java/com/mediaview/fastdfs/web/rest/ApiRestController.java
@@ -0,0 +1,38 @@
+package com.mediaview.fastdfs.web.rest;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+/**
+ * @program: s-3-fast-dfs
+ *
+ * @description: s3 api
+ *
+ * @author: LiXuemin
+ *
+ * @create: 2020-04-29
+ **/
+public class ApiRestController {
+    @GetMapping("/")
+    public String getObject() {
+        return null;
+    }
+
+    @PostMapping("/")
+    public String postObject() {
+        return null;
+    }
+
+    @PutMapping("/")
+    public String putObject() {
+        return null;
+    }
+    @DeleteMapping("/")
+    public String deleteObject() {
+        return null;
+    }
+
+}
