f3b0680f62a8d4980fce4b86b21e4e2d7f19e21e
lixuemin <lxmheida@gmail.com>
2021-09-22 22:37:44 +0800

    feature: add rocketmq consumer

--- text-converter/src/main/java/com/lxm/textconverter/config/Constants.java
+++ text-converter/src/main/java/com/lxm/textconverter/config/Constants.java
@@ -11,15 +11,18 @@ public final class Constants {
     public static final String CONVERT_STATE_SUCCESS = String.valueOf(CONVERT_STATE.SUCCESS);
     public static final String CONVERT_STATE_FAILED = String.valueOf(CONVERT_STATE.FAILED);
     public static final String PRODUCER_GROUP_TEXT_CONVERT = "PRODUCER-GROUP-TEXT-CONVERT";
+    public static final String CONSUMER_GROUP_TEXT_CONVERT = "CONSUMER-GROUP-TEXT-CONVERT";
     public static final String NAMESERV_ADDR = "localhost:9876";
     public static final String TOPIC_TEXT_CONVERT = "TOPIC_CONVERT_TEXT";
-    private Constants() {
-    }
+
 
     /**
      * state of text convert
      */
     public enum CONVERT_STATE {
         NEW, RUNNING, CANCELED, SUCCESS, FAILED
+    }
+
+    private Constants() {
     }
 }
